4617ceac22dc7ee27143efe02f29ce6172752f35
amasiero <amasiero@tripadvisor.com>
2022-01-13 19:21:56 -0300

    Communication using RestTemplate

--- hr-payroll/src/main/java/com/andreymasiero/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/andreymasiero/hrpayroll/services/PaymentService.java
@@ -1,11 +1,31 @@
 package com.andreymasiero.hrpayroll.services;
 
 import com.andreymasiero.hrpayroll.entities.Payment;
+import com.andreymasiero.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+    private final RestTemplate restApi;
+
+    @Autowired
+    public PaymentService(final RestTemplate restApi) {
+        this.restApi = restApi;
+    }
+
     public Payment getPayment(Long workerId, Integer days) {
-        return new Payment("Gloria Maria", 200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", workerId.toString());
+        var worker = restApi.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
