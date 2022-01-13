dfe1fe8e25b9db94ab5ce52e9c6bd75ce9e3f711
amasiero <amasiero@tripadvisor.com>
2022-01-13 20:10:30 -0300

    Refactor RestTemplate to FeignReign

--- hr-payroll/src/main/java/com/andreymasiero/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/andreymasiero/hrpayroll/services/PaymentService.java
@@ -1,31 +1,25 @@
 package com.andreymasiero.hrpayroll.services;
 
 import com.andreymasiero.hrpayroll.entities.Payment;
-import com.andreymasiero.hrpayroll.entities.Worker;
+import com.andreymasiero.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
     @Value("${hr-worker.host}")
     private String workerHost;
-    private final RestTemplate restApi;
+    private final WorkerFeignClient workerFeignClient;
 
     @Autowired
-    public PaymentService(final RestTemplate restApi) {
-        this.restApi = restApi;
+    public PaymentService(final WorkerFeignClient workerFeignClient) {
+        this.workerFeignClient = workerFeignClient;
     }
 
     public Payment getPayment(Long workerId, Integer days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", workerId.toString());
-        var worker = restApi.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        var worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
